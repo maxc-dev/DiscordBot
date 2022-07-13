832dcb9a19f6e05b38b2998414b9c9ad4a5fbd30
maxc-dev <mer.cartermc@gmail.com>
2022-07-13 16:37:59 +0100

    Updated coroutine flow for Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,25 @@
 import input.InputScanner
+import kotlinx.coroutines.runBlocking
 
-suspend fun main() {
-    Main().start()
+fun main() {
+    Launcher().start()
 }
 
-class Main {
+class Launcher {
     private val log = logger(this.javaClass)
 
-    suspend fun start() {
+    fun start() = runBlocking {
         log.info("Process started")
         val token = TokenRetriever().retrieveToken()
-        val botMain = BotMain(token)
 
-        botMain.start()
+        val botMain = BotMain(token)
+        botMain.start() // start the bot
+
         val inputScanner = InputScanner(botMain)
         inputScanner.listen()
 
         // application closes when inputScanner.listen() returns
         botMain.stop()
+        log.info("Process stopped")
     }
 }
